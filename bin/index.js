8b973202ed0ad542907d4e4b7dd6d1e72cbda037
Tom Kenny <tom@twome.name>
2019-09-28 01:24:32 +0800

    Better

--- bin/index.js
+++ bin/index.js
@@ -1,76 +1,163 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process')
+const { exec, execSync } = require('child_process')
+let { readdir } = require('fs')
 const { argv } = process
-const { info, debug } = console
+const { info, debug, error } = console
+const print = msg => process.stdout.write(msg)
 
 const chokidar = require('chokidar')
 const { redUnderline, green, pink, underline, yellow } = require('../colouring.js')
 
+readdir = require('util').promisify(readdir)
+
+;(async ()=>{
+
 const useExeDir = false
 const argsToProgram = argv.slice(1)
-const executeOutput = argsToProgram.includes('--execute-output')
-const classesToExecute = executeOutput ? argv.slice(argv.indexOf('--execute-output') + 1) : []
-info(`Executing classes: ${classesToExecute.join(', ')}`)
 
-const sourceFileWatcher = chokidar.watch('**/*.java', {
-	ignored: [
-		/(^|[/\\])\../ // dotfiles
-	],
+// C-L arguments
+const willCompileTests = argsToProgram.includes('--compile-tests')
+const willOnlyTest = argsToProgram.includes('--only-run-tests')
+const willRunTests = argsToProgram.includes('--run-tests') || willOnlyTest
+const willExecuteOutput = argsToProgram.includes('--execute-output') && !willRunTests
+
+const classesToExecute = willExecuteOutput ? argv.slice(argv.indexOf('--execute-output') + 1) : []
+if (willExecuteOutput) info(`Executing classes: ${classesToExecute.join(', ')}`)
+
+
+const runHarness = moduleName => {
+	// TODO deduplicate / throttle
+	exec(`java -enableassertions ${moduleName}TestHarness`, (nodeError, stdout, stderr) => {
+		if (nodeError){
+			error(`=== J-SOURCE-WATCHER ERROR ===`)
+			error(pink(nodeError))
+			return false
+		}
+		print(stdout)
+		process.stderr.write(pink(stderr))
+	})
+}
+
+
+
+let sourcesToWatch = '**/*.java'
+let ignored = [
+	/(^|[/\\])\../, // dotfiles
+	/(^|[/\\])no-compile/
+]
+if (!willCompileTests){
+	ignored.push(/TestHarness\.java$/)
+	ignored.push(/\/test\/$/)
+}
+const sourceFileWatcher = chokidar.watch(sourcesToWatch, {
+	ignored,
 	persistent: true
 })
 
 let compilationProcesses = new Map()
 
-const handler = (path, stats) => {
+const sourceHandler = (path, stats) => {
 	let timeSansMs = (new Date()).toISOString().replace('T',' ').split('.')[0]
-	console.info(`${yellow(timeSansMs)} ~ ${underline(path)} changed; compiling${executeOutput ? ' and running' : ''}...`)
+	console.info(`${yellow(timeSansMs)} ~ ${underline(path)} changed; compiling${willExecuteOutput ? ' and running' : ''}...`)
 
-	let srcName = path.replace(/\.java$/, '')
+	let moduleName = path.replace(/\.java$/, '')
 	let outputDirFlag = useExeDir ? ' -d bin' : ''
 
 	let hitCompilerError = false
 
-	let compilationProcess = exec(`javac ${srcName}.java${outputDirFlag}`, err => {
+	let compilation = exec(`javac ${moduleName}.java${outputDirFlag}`, err => {
 		if (!err) return
 		hitCompilerError = true
-		console.error(redUnderline(`Compiler exited with code: ${err.code}`))
+		console.error(redUnderline(`${moduleName} compilation exited with code: ${err.code}`))
 	})
 
-	let { pid } = compilationProcess
-	compilationProcesses.set(pid, { name: srcName, startTime: new Date() })
+	let { pid } = compilation
+	compilationProcesses.set(pid, {
+		name: moduleName,
+		startTime: new Date()
+	})
 
+	compilation.stdout.on('end', () => {
+		const processInfo = compilationProcesses.get(pid)
+		compilationProcesses.delete(pid)
+		const executionTime = (new Date() - processInfo.startTime) / 1000
 
-	compilationProcess.stdout.on('end', () => {
-		const executionTime = (new Date() - compilationProcesses.get(pid).startTime) / 1000
+		if (!hitCompilerError){
+			print('\n' + green(`[\``) + processInfo.name + green(`\` compiled with no errors after `) + `${executionTime}s` + green(`]`))
+			if (compilationProcesses.size > 0){
+				print(` (${compilationProcesses.size} remaining.)`)
+			} else {
+				print(` (Compilation done.)`)
+			}
+			print('\n')
+		}
 
-		if (!hitCompilerError) console.info(green(`[\``) + compilationProcesses.get(pid).name + green(`\` compiled with no errors after `) + `${executionTime}s` + green(`]`))
+		if (willExecuteOutput && !hitCompilerError && classesToExecute.includes(moduleName)){
+			console.info(`Running ${moduleName}…`);
 
-		if (executeOutput && !hitCompilerError && classesToExecute.includes(srcName)){
-			console.info(`Running ${srcName}…`);
-
-			let executionProcess = exec(`java -enableassertions ${srcName}`, err => {
+			let execution = exec(`java -enableassertions ${moduleName}`, err => {
 				if (!err) return
 				console.error(redUnderline(`JVM exited with code: ${err.code}`))
 			})
 
-			executionProcess.stdout.on('data', data => {
-				process.stdout.write(green(data))
+			execution.stdout.on('data', data => {
+				print(green(data))
 			})
 
-			executionProcess.stderr.on('data', data => {
+			execution.stderr.on('data', data => {
 				process.stderr.write(pink(data))
 			})
 		}
 	})
 
-	compilationProcess.stderr.on('data', data => {
+	compilation.stderr.on('data', data => {
 		hitCompilerError = true
 		process.stderr.write(pink(data))
 	})
 }
 
-sourceFileWatcher
-	.on('add', handler) // We need this to compile & run when first running this JS module
-	.on('change', handler)
-	.on('unlink', handler)
+if (!willOnlyTest){
+	sourceFileWatcher
+		.on('add', sourceHandler) // We need this to compile & run when first running this JS module
+		.on('change', sourceHandler)
+		.on('unlink', sourceHandler)
+}
+
+
+
+let harnessNames = new Set()
+
+if (willRunTests) {
+	let filenames = await readdir(process.cwd())
+
+	harnesses = filenames.filter(name => name.match(/TestHarness\.java$/))
+	for (let filename of harnesses){
+		let moduleName = filename.replace(/\.(class|java)$/, '').replace(/TestHarness$/, '')
+		harnessNames.add(moduleName)
+	}
+
+	// Watch the subject module code
+	chokidar.watch([...harnessNames].map(name => name + '.java'), { persistent: true })
+		.on('add', path => harnessHandler(path))
+		.on('change', path => harnessHandler(path))
+		.on('unlink', path => harnessHandler(path))
+
+	// Watch the test output .class files.
+	chokidar.watch([...harnessNames].map(name => name + 'TestHarness.class'), { persistent: true })
+		.on('add', path => harnessHandler(path))
+		.on('change', path => harnessHandler(path))
+		.on('unlink', path => harnessHandler(path))
+}
+
+const harnessHandler = (path, stats) => {
+	let moduleName = path.replace(/\.(class|java)$/, '').replace(/TestHarness$/, '')
+
+	harnessNames.add(moduleName) // For new ones creates after starting this up
+
+	runHarness(moduleName)
+}
+
+
+
+})() // async IIFE
--- colouring.js
+++ colouring.js
@@ -10,6 +10,6 @@ const redUnderline = string => colorSequence('31;1') + string + clearColors
 const green = string => colorSequence('32') + string + clearColors
 const pink = string => colorSequence('35') + string + clearColors
 const underline = string => colorSequence('0;4') + string + clearColors
-const yellow = string => colorSequence('40') + string + clearColors
+const yellow = string => colorSequence('33') + string + clearColors
 
 module.exports = { redUnderline, green, pink, underline, yellow }
